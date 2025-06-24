8b85e7152dbb0506eef85f9be02de0f38efb75e1
vlad <v.martyanov@etpgpb.ru>
2025-06-24 19:59:05 +0300

    migrate to blink.cmp, add blink-cmp-copilot, remove nvim-cmp

--- lua/plugins/ai-assistants.lua
+++ lua/plugins/ai-assistants.lua
@@ -1,31 +1,12 @@
 return {
     {
-        "github/copilot.vim",
-        event = "VeryLazy",
-        enabled = false,
-        config = function()
-            -- Отключаем стандартный Tab mapping
-            vim.g.copilot_no_tab_map = true
-
-            -- Маппим принятие предложения на Ctrl+j
-            vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', {
-                expr = true,
-                replace_keycodes = false,
-            })
-
-            -- Дополнительные полезные маппинги
-            vim.keymap.set("i", "<C-l>", "<Plug>(copilot-accept-word)") -- принять слово
-            vim.keymap.set("i", "<C-;>", "<Plug>(copilot-next)") -- следующее предложение
-            vim.keymap.set("i", "<C-,>", "<Plug>(copilot-previous)") -- предыдущее предложение
-            vim.keymap.set("i", "<C-\\>", "<Plug>(copilot-dismiss)") -- отклонить предложение
-        end,
-    },
-    {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
         config = function()
-            require("copilot").setup {}
+            require("copilot").setup {
+                suggestion = { enabled = false },
+            }
         end,
     },
 }
--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,13 +1,25 @@
 return {
-    -- { import = "nvchad.blink.lazyspec" },
-    -- {
-    --     "saghen/blink.cmp",
-    --     enabled = true,
-    --     event = { "LspAttach", "InsertEnter" },
-    --     opts = {
-    --         sources = {
-    --             default = { "copilot", "lsp", "path", "luasnip", "buffer" },
-    --         },
-    --     },
-    -- },
+    { import = "nvchad.blink.lazyspec" },
+    {
+        "saghen/blink.cmp",
+        enabled = true,
+        event = { "LspAttach", "InsertEnter" },
+        opts = {
+            sources = {
+                default = { "copilot", "lsp", "path", "snippets", "buffer" },
+                providers = {
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                        score_offset = 100,
+                        async = true,
+                    },
+                },
+            },
+        },
+    },
+    {
+        "giuxtaposition/blink-cmp-copilot",
+        after = { "copilot.lua" },
+    },
 }
