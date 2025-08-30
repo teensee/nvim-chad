c4b4905363fc0eac016532b1752d2b4bdbb4caa2
vlad <v.martyanov@etpgpb.ru>
2025-08-30 11:29:15 +0300

    fix jumping cursor issue

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,9 +4,24 @@ return {
     event = "VeryLazy",
     opts = {
         cmdline = { enabled = true },
-        messages = { enabled = true }, -- conflict with go package
+        messages = { enabled = true },
         popupmenu = { enabled = true },
         notify = { enabled = true },
+
+        lsp = {
+            progress = {
+                enabled = true, -- прогресс LSP можно оставить
+                format = "lsp_progress",
+                format_done = "lsp_progress_done",
+                throttle = 1000 / 30,
+                view = "mini",
+            },
+            -- override = {},
+            hover = { enabled = false },
+            signature = { enabled = false },
+            message = { enabled = false },
+            documentation = { enabled = false },
+        },
     },
     dependencies = {
         "MunifTanjim/nui.nvim",
